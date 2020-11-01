e5a31488c68fdd80bcea94fd79730e809eee8d87
Suro Zakaryan <suren.zakaryan177@gmail.com>
2020-11-02 00:43:10 +0400

    change commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Login} from './components/Login/Login';
 import Route from "react-router-dom/es/Route";
-import { Header } from './components/Header/Header';
+import {Header} from './components/Header/Header';
 import ContentContainer from './components/Content/ContentContainer';
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import NewsContainer from "./components/News/NewsCategories/Bitcoin/BitcoinContainer";
@@ -10,19 +10,22 @@ import './index.css'
 import BusinessContainer from "./components/News/NewsCategories/Business/BusinessContainer";
 import AppleContainer from "./components/News/NewsCategories/Apple/AppleContainer";
 import TechCrunchContainer from "./components/News/NewsCategories/TechCrunch/TechCrunchContainer";
+import {ParallaxProvider} from "react-scroll-parallax";
 
 function App() {
     return (
-        <div className="App">
-            <Route path='/login' render={() => <Login />}/>
-            <Route path='/content' render={() => <ContentContainer />}/>
-            <Route path='/header' render={() => <Header /> }/>
-            <Route path='/profile/:userId' render={() => <ProfileContainer /> }/>
-            <Route path='/Bitcoin' render={() => <NewsContainer /> }/>
-            <Route path='/Business' render={() => <BusinessContainer />}/>
-            <Route path='/Apple' render={() => <AppleContainer />}/>
-            <Route path='/TechCrunch' render={() => <TechCrunchContainer />}/>
-        </div>
+        <ParallaxProvider>
+            <div className="App">
+                <Route path='/login' render={() => <Login/>}/>
+                <Route path='/content' render={() => <ContentContainer/>}/>
+                <Route path='/header' render={() => <Header/>}/>
+                <Route path='/profile/:userId' render={() => <ProfileContainer/>}/>
+                <Route path='/Bitcoin' render={() => <NewsContainer/>}/>
+                <Route path='/Business' render={() => <BusinessContainer/>}/>
+                <Route path='/Apple' render={() => <AppleContainer/>}/>
+                <Route path='/TechCrunch' render={() => <TechCrunchContainer/>}/>
+            </div>
+        </ParallaxProvider>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 import store from "./components/Redux/redux-store";
 
 ReactDOM.render(
     <BrowserRouter>
-    <Provider store={store}>
-        <React.StrictMode>
-            <App/>
-        </React.StrictMode>
-    </Provider>
+        <Provider store={store}>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </Provider>
     </BrowserRouter>,
-document.getElementById('root')
+    document.getElementById('root')
 );
 
 serviceWorker.unregister();
