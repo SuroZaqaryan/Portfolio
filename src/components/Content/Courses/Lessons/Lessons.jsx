507d1058f3122377978ba826ad9e8ed2b1866d8e
Suro Zakaryan <suren.zakaryan177@gmail.com>
2020-12-05 11:36:18 +0400

    add Lesson page

--- src/components/Content/Courses/Lessons/Lessons.jsx
+++ src/components/Content/Courses/Lessons/Lessons.jsx
@@ -1,52 +1,76 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import less from "./css/lesson.module.css";
+import "./css/activeLink.css";
 import "./css/betaLesson.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Navbar } from "../../../Navbar/Navbar";
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 export class Lessons extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            indexDescription: 0,
+            indexDescription: 1,
+            listActiveIndex: 1,
         }
+    }
+
+    changeDescription(index) {
+        this.setState({ indexDescription: index })
+
+        // active link
+        this.setState({ listActiveIndex: index });
     }
 
     render() {
         const listLessons = this.props.lesson.map((item, index) => {
-            return (
-                <ul key={index}>
-                    <li className={less.sidebar_list} onClick={() => {
-                        this.setState({ indexDescription: index })
-                    }}>
-                        <div>
-                            <FontAwesomeIcon className={less.item_icon} icon={item.iconName} />
-                        </div>
 
-                        <div className={less.titleName}>
-                            <p>{item.titleName}</p>
-                        </div>
-                    </li>
-                </ul>
+            // active link
+            const className = this.state.listActiveIndex === index ? 'list_active' : null;
+
+            return (
+                <Fragment key={index}>
+
+                    {item.title && (
+                        <li className={less.courseTitle}>
+                            <div>
+                                <p>{item.title}</p>
+                            </div>
+                        </li>
+                    )}
+
+                    {item.titleName && (
+                        <li className={className} onClick={this.changeDescription.bind(this, index)}>
+                            <div className={less.sidebar_list}>
+                                <div style={{ display: "flex" }}>
+                                    <FontAwesomeIcon className={less.item_icon} icon={item.iconName} />
+                                </div>
+                                <div className={less.titleName}>
+                                    <div>
+                                        <p>{item.titleName}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    )}
+                </Fragment>
             );
         });
-
 
         return (
             <>
                 <div className="abc">
                     <Navbar color="blue" bg="tomato" centerFlexNavbarContainer="flex"
-                    navbarSearchPage="Search" navbarHomePage="Home" centerHeadlineNavbarColumn="center"/>
+                        navbarSearchPage="Search" navbarHomePage="Home" centerHeadlineNavbarColumn="center" />
                     <div className={less.wrapper}>
 
-                        <div className={less.sidebar}>
-                            {/*<div className={less.logo}>*/}
-                            {/*    <img src={bg} alt=""/>*/}
-                            {/*</div>*/}
-                            <div>
-                                <ul>
-                                    {listLessons}
-                                </ul>
+                        <div>
+                            <div className={less.sidebar}>
+                                <div>
+                                    <ul>
+                                        {listLessons}
+                                    </ul>
+                                </div>
                             </div>
                         </div>
 
@@ -54,14 +78,14 @@ export class Lessons extends React.Component {
                             <div className={less.main_inside_content}>
                                 <div className={less.header}>
                                     <div>
-                                        <h2>JavaScript JSON Reference</h2>
+                                        <h2>{this.props.lesson[this.state.indexDescription]["heading"]}</h2>
                                     </div>
                                 </div>
                                 <div className={less.info}>
                                     <div className={less.description}>
                                         <p>
                                             {
-                                                this.props.lesson[this.state.indexDescription]["description"]
+                                                ReactHtmlParser(this.props.lesson[this.state.indexDescription]["description"])
                                             }
                                         </p>
                                     </div>
--- src/components/Redux/lesson-reducer.js
+++ src/components/Redux/lesson-reducer.js
@@ -1,48 +1,10 @@
 import React from 'react';
-import {faDotCircle} from "@fortawesome/free-solid-svg-icons/faDotCircle";
+import JavaScriptLessonObject from "./Coding_Lessons_Objects/JavaScript_Lessons_Objects";
 
 const ADD_LESSON = "ADD_LESSON";
-const one = "Robby";
 
 let initialState = {
-    lessonsTitle: [
-        {
-            titleName: "JSON",
-            iconName: faDotCircle,
-            description: [
-                <span className="yourClass">{one}</span>,
-                ` advanced diverted domestic sex repeated bringing you old.
-                Possible procured her trifling laughter`
-            ],
-            languages: [
-                'Язык HTML1',
-            ]
-        },
-        {
-            titleName: "Boolean",
-            iconName: faDotCircle,
-            description: `
-                Folly words widow one downs few age every seven.
-                If miss part by fact he park just shew.
-                Discovered had get considered projection who favourable. Necessary up knowledge it tolerably.
-            `,
-            languages: [
-                'Язык HTML2',
-            ]
-        },
-        {
-            titleName: "Math",
-            iconName: faDotCircle,
-            description: `
-               Another journey chamber way yet females man.
-               Way extensive and dejection get delivered deficient sincerity gentleman age.
-               Too end instrument possession contrasted motionless. Calling offence six joy feeling.
-            `,
-            languages: [
-                'Язык HTML3',
-            ]
-        },
-    ]
+    lessonsTitle: JavaScriptLessonObject()
 };
 
 export const lessonReducer = (state = initialState, action) => {
